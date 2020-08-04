b61074fc8265a0302c2892da49840854f1fd434b
Serhii <ragazzo@ukr.net>
2020-08-04 09:12:13 +0300

    BaseTestCreated

--- src/test/java/TestConfig.java
+++ src/test/java/TestConfig.java
@@ -5,6 +5,7 @@ public class TestConfig {
 
     @BeforeClass
     public static void setup(){
+        RestAssured.proxy("localhost", 8888); // it setting for fiddler you should install fiddler on your pc
         RestAssured.baseURI = "http://video-game-db.eu-west-2.elasticbeanstalk.com";
         RestAssured.basePath = "/app/";
 
