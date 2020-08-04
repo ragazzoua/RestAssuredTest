a20c6b934b3270624c0044315298682d23f12f28
Serhii <ragazzo@ukr.net>
2020-08-04 09:22:50 +0300

    RequestSpecification added

--- src/test/java/TestConfig.java
+++ src/test/java/TestConfig.java
@@ -1,13 +1,22 @@
 import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
 import org.junit.BeforeClass;
 
 public class TestConfig {
 
     @BeforeClass
-    public static void setup(){
+    public static void setup() {
         //RestAssured.proxy("localhost", 8888); // it is  setting for fiddler you should install fiddler on your pc
         RestAssured.baseURI = "http://video-game-db.eu-west-2.elasticbeanstalk.com";
         RestAssured.basePath = "/app/";
+
+        RequestSpecification requestSpecification = new RequestSpecBuilder()
+                .addHeader("Content-Type", "application/json")
+                .addHeader("Accept", "application/json")
+                .build();
+
+        RestAssured.requestSpecification = requestSpecification;
 
     }
 }
