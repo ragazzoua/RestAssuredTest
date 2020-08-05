eb4d4116cdca44795a678350b1c096d13d28e029
Serhii <ragazzo@ukr.net>
2020-08-05 21:34:51 +0300

    createNewGameByJson test added

--- src/test/java/VideoGameDbTest.java
+++ src/test/java/VideoGameDbTest.java
@@ -7,8 +7,28 @@ import static io.restassured.RestAssured.given;
 public class VideoGameDbTest extends VideoGameConfig {
 
     @Test
-    public void getAllGames(){
+    public void getAllGames() {
         given().when().get(VideoGamesEndpoints.ALL_VIDEO_GAMES).then().log().all();
+    }
+
+    @Test
+    public void createNewGameByJson() {
+        String gameBodyJson = "{\n" +
+                "  \"id\": 112,\n" +
+                "  \"name\": \"MyNewGame\",\n" +
+                "  \"releaseDate\": \"2020-08-05T18:27:11.397Z\",\n" +
+                "  \"reviewScore\": 88,\n" +
+                "  \"category\": \"Shooter\",\n" +
+                "  \"rating\": \"Mature\"\n" +
+                "}";
+
+        given()
+                .body(gameBodyJson)
+                .when()
+                .post(VideoGamesEndpoints.ALL_VIDEO_GAMES)
+                .then()
+                .log()
+                .all();
     }
 
 }
