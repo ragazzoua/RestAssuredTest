965c8d2360500fc98cb656eff3ac3281a31c01cf
Serhii <ragazzo@ukr.net>
2020-08-06 08:41:03 +0300

    getDateFounded Test added

--- src/test/java/FootballApiTests.java
+++ src/test/java/FootballApiTests.java
@@ -2,15 +2,26 @@ import config.FootballApiConfig;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
 
 public class FootballApiTests extends FootballApiConfig {
 
     @Test
-    public void getDetailOfOneArea(){
+    public void getDetailOfOneArea() {
         given()
                 .queryParam("areas", 2255)
                 .when()
-                .get("areas")
-        ;
+                .get("areas");
     }
+
+    @Test
+    public void getDateFounded() {
+        given()
+                .when()
+                .get("teams/57")
+                .then()
+                .body("founded", equalTo(1886));
+    }
+
+
 }
