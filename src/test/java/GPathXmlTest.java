1db9ac84d085c8dc8b7e1229ece8b22eadfa649a
Serhii <ragazzo@ukr.net>
2020-08-09 10:51:44 +0300

    GPath XML Part 3 - Extract all XML nodes with findAll

--- src/test/java/GPathXmlTest.java
+++ src/test/java/GPathXmlTest.java
@@ -1,24 +1,38 @@
 import config.VideoGamesEndpoints;
+import io.restassured.path.xml.XmlPath;
+import io.restassured.path.xml.element.Node;
 import io.restassured.response.Response;
 import org.junit.Test;
+
+import java.util.List;
 
 import static io.restassured.RestAssured.get;
 
 public class GPathXmlTest extends TestConfig {
 
     @Test
-    public void getFirstGameInTheList(){
+    public void getFirstGameInTheList() {
         Response response = get(VideoGamesEndpoints.ALL_VIDEO_GAMES);
 
-        String name  = response.path("videoGames.videoGame.name[0]");
+        String name = response.path("videoGames.videoGame.name[0]");
         System.out.println(name);
     }
 
     @Test
-    public void getAttributeName(){
+    public void getAttributeName() {
         Response response = get(VideoGamesEndpoints.ALL_VIDEO_GAMES);
-        String category  = response.path("videoGames.videoGame[1].@category");
+        String category = response.path("videoGames.videoGame[1].@category");
 
         System.out.println(category);
+    }
+
+    @Test
+    public void getListOfXmlNodes(){
+        String response = get(VideoGamesEndpoints.ALL_VIDEO_GAMES).asString();
+
+        List<Node> nodes = XmlPath.from(response)
+                .get("videoGames.videoGame.findAll { element -> return element}");
+
+        System.out.println(nodes.get(5).get("name").toString());
     }
 }
